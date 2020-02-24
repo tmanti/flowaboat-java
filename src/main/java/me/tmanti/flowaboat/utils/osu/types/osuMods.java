68be4251957010c13add5593109e661e283e428a
zivoy <ziv132@gmail.com>
2020-02-23 22:27:25 -0500

    add and remove methods

--- src/main/java/me/tmanti/flowaboat/utils/osu/types/osuMods.java
+++ src/main/java/me/tmanti/flowaboat/utils/osu/types/osuMods.java
@@ -8,48 +8,52 @@ import java.util.regex.Pattern;
 public class osuMods {
     private static final String[] DIFF_MODS = new String[]{"HR", "EZ", "DT", "HT", "NC", "FL", "HD", "NF"};
     private static final String[] TIME_MODS = new String[]{"DT", "HT", "NC"};
-    private static final Map<String, Integer> MOD_FLAGS = new HashMap<String, Integer>() {{
-        put("NF", 1 << 0);
-        put("EZ", 1 << 1);
-        put("TD", 1 << 2);
-        put("HD", 1 << 3);
-        put("HR", 1 << 4);
-        put("SD", 1 << 5);
-        put("DT", 1 << 6);
-        put("RX", 1 << 7);
-        put("HT", 1 << 8);
-        put("NC", 1 << 9);
-        put("FL", 1 << 10);
-        put("AT", 1 << 11);
-        put("SO", 1 << 12);
-        put("AP", 1 << 13);
-        put("PF", 1 << 14);
-        put("4K", 1 << 15);
-        put("5K", 1 << 16);
-        put("6K", 1 << 17);
-        put("7K", 1 << 18);
-        put("8K", 1 << 19);
-        put("FI", 1 << 20);
-        put("RD", 1 << 21);
-        put("LM", 1 << 22);
-        put("TR", 1 << 23);
-        put("9K", 1 << 24);
-        put("10K", 1 << 25);
-        put("1K", 1 << 26);
-        put("3K", 1 << 27);
-        put("2K", 1 << 28);
-        put("V2", 1 << 29);
+    private static final Map<String, Long> MOD_FLAGS = new HashMap<String, Long>() {{
+        put("NF", 1L << 0);
+        put("EZ", 1L << 1);
+        put("TD", 1L << 2);
+        put("HD", 1L << 3);
+        put("HR", 1L << 4);
+        put("SD", 1L << 5);
+        put("DT", 1L << 6);
+        put("RX", 1L << 7);
+        put("HT", 1L << 8);
+        put("NC", 1L << 9);
+        put("FL", 1L << 10);
+        put("AT", 1L << 11);
+        put("SO", 1L << 12);
+        put("AP", 1L << 13);
+        put("PF", 1L << 14);
+        put("4K", 1L << 15);
+        put("5K", 1L << 16);
+        put("6K", 1L << 17);
+        put("7K", 1L << 18);
+        put("8K", 1L << 19);
+        put("FI", 1L << 20);
+        put("RD", 1L << 21);
+        put("LM", 1L << 22);
+        put("TR", 1L << 23);
+        put("9K", 1L << 24);
+        put("10K", 1L << 25);
+        put("1K", 1L << 26);
+        put("3K", 1L << 27);
+        put("2K", 1L << 28);
+        put("V2", 1L << 29);
     }};
-    private static final Map<Integer, String> MOD_FLAGS_INV = new HashMap<Integer, String>() {{
-        for (Map.Entry<String, Integer> entry : MOD_FLAGS.entrySet()) {
+    private static final Map<Long, String> MOD_FLAGS_INV = new HashMap<Long, String>() {{
+        for (Map.Entry<String, Long> entry : MOD_FLAGS.entrySet()) {
             put(entry.getValue(), entry.getKey());
         }
     }};
     private static final Pattern MODS_RE = Pattern.compile(generateModString());
-    public int modFlag;
+    public Long modFlag;
+
+    public osuMods(Long mods) {
+        this.modFlag = addMissing(mods);
+    }
 
     public osuMods(int mods) {
-        this.modFlag = mods;
+        this((long) mods);
     }
 
     public osuMods() {
@@ -71,8 +75,8 @@ public class osuMods {
         return string.toString();
     }
 
-    public static int getModInt(String mod) {
-        if (!MOD_FLAGS.containsKey(mod)) return 0;
+    public static Long getModInt(String mod) {
+        if (!MOD_FLAGS.containsKey(mod)) return 0L;
         return MOD_FLAGS.get(mod.toUpperCase());
     }
 
@@ -87,7 +91,7 @@ public class osuMods {
 
         int mods = 0;
         while (mods_included.find()) {
-            int curr = getModInt(mods_included.group());
+            Long curr = getModInt(mods_included.group());
             if ((mods & curr) == 0) {
                 mods += curr;
             }
@@ -97,12 +101,54 @@ public class osuMods {
 
     public String getModString() {
         StringBuilder string = new StringBuilder();
-        for (int i : MOD_FLAGS_INV.keySet()) if ((this.modFlag & i) != 0) string.append(MOD_FLAGS_INV.get(i));
+        for (Long i : MOD_FLAGS_INV.keySet()) if ((this.modFlag & i) != 0) string.append(MOD_FLAGS_INV.get(i));
         return string.toString();
     }
 
-    public int getModInt() {
+    public Long getBitWiseFlag() {
         return this.modFlag;
+    }
+
+    public boolean contains(String mod) {
+        return (this.modFlag & getModInt(mod)) != 0;
+    }
+    public boolean contains(long mod) {
+        return (this.modFlag & mod) != 0;
+    }
+    public boolean contains(int mod) {
+        return this.contains((long) mod);
+    }
+
+    private long addMissing(Long modInt) {
+        if (this.contains("NC") && !this.contains("DT")) modInt += getModInt("DT");
+        if (this.contains("PF") && !this.contains("SD")) modInt += getModInt("SD");
+        return modInt;
+    }
+
+    public void add(String mods) {
+        this.add(getModInt(mods));
+    }
+    public void add(int mods) {
+        this.add((long) mods);
+    }
+    public void add(osuMods mods) {
+        this.add(mods.getBitWiseFlag());
+    }
+    public void add(long mods) {
+        this.modFlag = addMissing(this.modFlag | mods);
+    }
+
+    public void remove(String mods) {
+        this.remove(getModInt(mods));
+    }
+    public void remove(int mods) {
+        this.remove((long) mods);
+    }
+    public void remove(osuMods mods) {
+        this.remove(mods.getBitWiseFlag());
+    }
+    public void remove(long mods) {
+        this.modFlag = addMissing((this.modFlag & ~mods));
     }
 
 
